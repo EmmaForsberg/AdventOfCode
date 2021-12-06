70c7d93c2963dc16b53d51f672a8d0e9716c324d
Emma Forsberg <32894170+EmmaForsberg@users.noreply.github.com>
2021-12-06 15:16:44 +0100

    Dayy 3 part one finish

--- Day3.cs
+++ Day3.cs
@@ -14,6 +14,26 @@ namespace AdventOfCode
             ReadFileInput fileInput = new ReadFileInput();
             string[] lines = fileInput.ReadFile();
 
+            var lengthofline = lines[0].Length;
+
+            char[] gamma = new char[lengthofline];
+            char[] epilson = new char[lengthofline];
+
+            for (int i = 0; i < lengthofline; i++) 
+            {
+                var number0 = lines.Select(x => x[i]).Count(x => x == '0');
+                var number1 = lines.Select(x => x[i]).Count(x => x == '1');
+
+                gamma[i] += (number0 > number1) ? '0' : '1';
+                epilson[i] += (number0 < number1) ? '0' : '1';
+
+            }
+            var gammarate = Convert.ToInt32(new String(gamma), 2);
+            var epilsonrate = Convert.ToInt32(new String(epilson), 2);
+
+            var result = gammarate * epilsonrate;
+            Console.WriteLine(result);
+
         }
     }
 }
